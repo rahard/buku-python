60ca7d281d5845babb22470f476cd8fcdd44b48e
Budi Rahardjo <rahard@gmail.com>
2018-11-10 12:28:01 +0700

    update pendahuluan

---
+++ contoh/list.py
@@ -0,0 +1,11 @@
+# contoh list di Python
+daftar = [7, 1, 5, 3, 2, 4, 6, 8, 3, 5]
+print(daftar)
+print("list memiliki", len(daftar), "elemen")
+
+# akses elemen yang pertama (dengan indeks 0)
+print("daftar[0] =", daftar[0])
+
+# menambahkan data ke list
+daftar = daftar + [7, 9]
+print(daftar)
