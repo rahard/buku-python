3bf248e7c73a9efad99145516e93d9aa98bdc558
Budi Rahardjo <rahard@gmail.com>
2024-10-20 12:17:34 +0700

    if then else
    menambahkan bagian branching

---
+++ contoh/negatif.py
@@ -0,0 +1,7 @@
+# contoh branching
+x = input("Masukkan bilangan antara -10 dan 10: ");
+z = int(x)
+if z < 0:
+    print("Anda memasukkan bilangan negatif, ", z)
+else:
+    print("Anda memasukkan bilangan positif, ", z)
